87b11d59f2e40ac7e720e7272095e8d6931f1cf3
Ayush-Shyam-Kumar <65535504+Ayush-Shyam-Kumar@users.noreply.github.com>
2020-05-18 23:24:33 +0530

    Create ArrayToBST.java

---
+++ ArrayToBST.java
@@ -0,0 +1,86 @@
+package DS_Project;
+public class ArrayToBST {
+	class Node {
+		int frq;
+		char chr;
+		int key;
+		Node left, right;
+		public Node(int f,char c) {
+			frq=f;
+			chr=c;
+			Character c1 = Character.valueOf(c);
+			key=c1.hashCode();
+			left = right = null;
+		}
+	}
+	Node root;
+	public Node insert(char chr) {
+		return root = insertReccursion(root, chr);
+	}
+
+	Node insertReccursion(Node root, char chr) {
+		if (root == null) {
+			root = new Node(1,chr);
+			return root;
+		}
+		Character c1 = Character.valueOf(chr);
+		int key=c1.hashCode();
+		if (key < root.key)
+			root.left = insertReccursion(root.left, chr);
+		else
+			root.right = insertReccursion(root.right, chr);
+		return root;
+	}
+	public void inorder() {
+		inorderReccursion(root);
+	}
+	void inorderReccursion(Node root) {
+		if (root != null) {
+			inorderReccursion(root.left);                  
+			System.out.print("\t"+root.chr+":"+root.frq);
+			inorderReccursion(root.right);
+		}
+	}
+    public Node search(Node root,char c) 
+    { 
+    	Character c1 = Character.valueOf(c);
+        int key=c1.hashCode();
+        while (root != null) { 
+            if (key > root.key) 
+                root = root.right; 
+            else if (key < root.key) 
+                root = root.left; 
+            else
+            {
+            return root;
+            }
+        } 
+        return null;
+    }
+	public boolean isEmpty()
+	{
+		return root==null;
+	}	
+	public void readArray(char[] c)
+	{   Node temp;
+		for(int i=0;i<c.length;i++)
+	{  
+		if(search(root,c[i])==null)
+		{   System.out.println(c[i]);
+			temp=insert(c[i]);
+		}
+		else {
+			System.out.println(c[i]);
+		 temp=search(root,c[i]);
+		 temp.frq+=1;
+		}
+	} 
+	}
+	public static void main(String[] args)
+	{
+		char[] a= {'m','a','l','a','y','a','l','a','m'};
+		ArrayToBST bst1=new ArrayToBST();
+		bst1.readArray(a);
+		bst1.inorder();
+	}
+}
